3ecd9b7a013463b867f6257e40dadfa4b37b2c2c
Nathalie KAUV <kauv@polytech.unice.fr>
2014-02-07 21:53:55 +0100

    un test de fait youhou !

--- test/search_test.py
+++ test/search_test.py
@@ -13,20 +13,56 @@ import unittest
 
 
 class searchTest(unittest.TestCase):
+    
+    nameArticle = "Article sur les saucisses"
+    nameAuthor = "Tomtom"
 
     def setUp(self):
         '''This method is run BEFORE EACH test.
         So we could initialize the database here.'''
-        pass # j'ai pas d'idée quoi initialiser pôur l'instant
+        
+        article = Article(name = nameArticle)
+        article.put()
+        
+        author = Author(name = nameAuthor)
+        author.put()
+        artiAuth = ArtiAuth(keyAuthor= author, keyArticle=article)
+        artiAuth.put()
+        
+        text = "La mère du maire est dans la mer; Le vers dans le verre est vert; j'ai mon thé sur la table;"
+        dataDict = mapper(text)
+        dataDict = reduce(dataDict)
+        
+        for cle in dataDict.keys():
+            mapReduce = MapReduce(keyWord = cle, keyArticle = article, count = dataDict[cle])
+            mapReduce.put()
+        
+            checkMaster = Master.all()
+            checkMaster.filter('keyWord =', cle)
+            if checkMaster.count() > 0 :
+                master = checkMaster.get()
+                master.count = master.count + dataDict[cle]
+            else :
+                master = Master(keyWord = cle, count=dataDict[cle])
+            master.put()
+        
+        #pass # j'ai pas d'idée quoi initialiser pôur l'instant
     
     def tearDown(self):
         '''This method is run AFTER EACH test.
         So we could clean the database after each test.'''
-        pass # j'ai pas d'idée quoi vider pour l'instant
+        deleteData()
+        #pass # j'ai pas d'idée quoi vider pour l'instant
     
     def testGetArticle(self):
         '''Adds articles and test the getArticle() search.'''
-        nameArticle = "Article sur les saucisses"
-        Article(name = nameArticle).put()
+        #nameArticle = "Article sur les saucisses"
+        #Article(name = nameArticle).put()
         result = getArticle(nameArticle)
         self.assertEquals(nameArticle,result.name)
+        
+    def testgetAuthorsByWords(self):
+        '''Check if the good author is return'''
+        words = "vers, verre"
+        result = getPaperByWords(words, 1)
+        self.assertEquals(result[0], author)
